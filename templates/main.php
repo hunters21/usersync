fe9d3894ff4d759416f239def9fcc5a7f8ebce03
hunters21 <92694894+hunters21@users.noreply.github.com>
2023-09-18 17:13:38 -0400

    main.php

--- templates/main.php
+++ templates/main.php
@@ -1,6 +1,12 @@
 <?php
-declare(strict_types=1);
-// SPDX-FileCopyrightText: Adam Kobke <akobke@yahoo.com>
+// SPDX-FileCopyrightText: Hunter Sanchez <hsanc053@fiu.edu>
 // SPDX-License-Identifier: AGPL-3.0-or-later
+declare(strict_types=1);
+script('myapp', 'script');  // it loads the js/script.js
 ?>
-<div id="content"></div>
+
+<div>
+    <input type="text" id="pathInput" placeholder="Enter the path">
+    <button id="submitBtn">Submit</button>
+    <div id="output"></div>
+</div>
