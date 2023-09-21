2e79dc1c37e3bb181a5d4a5339f680139438916d
hunters21 <92694894+hunters21@users.noreply.github.com>
2023-09-21 18:04:13 -0400

    Add files via upload

--- templates/main.php
+++ templates/main.php
@@ -2,11 +2,12 @@
 // SPDX-FileCopyrightText: Hunter Sanchez <hsanc053@fiu.edu>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 declare(strict_types=1);
-script('myapp', 'script');  // it loads the js/script.js
+script('myapp', 'script');
 ?>
 
-<div>
-    <input type="text" id="pathInput" placeholder="Enter the path">
-    <button id="submitBtn">Submit</button>
-    <div id="output"></div>
-</div>
+<form id="csvUploadForm">
+    <label for="csvFile">Upload CSV:</label>
+    <input type="file" name="csvFile" id="csvFile" accept=".csv">
+    <button type="button" id="uploadButton">Upload and Display</button>
+</form>
+<textarea id="csvOutput" rows="10" cols="50"></textarea>
